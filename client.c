484db0ffc71c560ec851f1f3d8c9cea4cefce38d
Oleg Doronin <dorooleg@niuitmo.ru>
2020-10-24 02:42:35 +0300

    initial template

---
+++ client.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dns_protocol.h"
+
+int main(int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,8 @@
+#include "dns_protocol.h"
+
+
+int main(int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
+    return 0;
+}
