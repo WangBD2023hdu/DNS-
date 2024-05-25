ab83e223c0f56c445625b07456dd9e480cb272c6
wbd <wbd@Ubuntu.myguest.virtualbox.org>
2024-05-25 17:04:08 +0800

    first commit2

--- client.cpp
+++ client.cpp
@@ -23,7 +23,7 @@ void init_header(struct DNS_HEADER *header) {
   header->q_count = htons(1);
 }
 
-uint32_t send_query(int sockfd, const char *domain, sockaddr_in *servaddr) {
+int send_query(int sockfd, const char *domain, sockaddr_in *servaddr) {
   char buf[65535];
   bzero(buf, 65535);
   struct DNS_HEADER header;
@@ -95,14 +95,14 @@ int main(int argc, char *argv[]) {
   if (sockfd < 0) {
     return -1;
   }
-  struct sockaddr_in servaddr = {0};
-  bzero((char *)&servaddr, sizeof(servaddr));
+  struct sockaddr_in servaddr;
+  bzero(&servaddr, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_port = htons(dns_server_port);
   bcopy(server->h_addr, (char *)&servaddr.sin_addr.s_addr,
         (size_t)server->h_length);
   printf("%s\n", server->h_addr);
-  uint32_t sendlen;
+  int sendlen;
   if ((sendlen = send_query(sockfd, domain, &servaddr)) == -1) {
     exit(1);
   }
